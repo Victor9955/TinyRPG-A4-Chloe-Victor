c04250a39b35919103c6f8d061fec441b44478a5
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-05-14 11:47:40 +0200

    Update

--- Assets/Script/Inventory/Inventory.cs
+++ Assets/Script/Inventory/Inventory.cs
@@ -1,6 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public abstract class Inventory : MonoBehaviour
 {
+    protected List<IItem> items;
 
+    public bool Add(IItem item)
+    {
+        return false;
+    }
+
+    public IItem Drop(int index)
+    {
+        if (items.Count >= index)
+        {
+            Drop();
+        }
+        return items[index];
+    }
+
+    public IItem Drop()
+    {
+        return items[0];
+    }
+
+    public List<IItem> DropAll()
+    {
+        return items;
+    }
 }
